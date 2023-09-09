a3aa2bbd062a5d1226d30327a453fba89834e349
lscambo13 <ls.cambo13@gmail.com>
2023-09-10 00:08:30 +0530

    dev: Mute log spam

--- js_modules/utils/intersectionObserver.js
+++ js_modules/utils/intersectionObserver.js
@@ -1,23 +1,23 @@
 export function intersectionObserver(rootID, targetClass) {
-    const targetClasses = document.getElementsByClassName(targetClass);
-    const options = {
-        root: document.getElementById(rootID),
-        rootMargin: '0px',
-        threshold: 0.5,
-    };
+	const targetClasses = document.getElementsByClassName(targetClass);
+	const options = {
+		root: document.getElementById(rootID),
+		rootMargin: '0px',
+		threshold: 0.5,
+	};
 
-    const callback = (entries) => {
-        entries.forEach((entry) => {
-            entry.isIntersecting ? localStorage
-                .setItem('selected-widget-style', entry.target.id) : null;
-            console.log(entry.target.id)
-            console.log(entry.intersectionRatio)
-        });
-    };
+	const callback = (entries) => {
+		entries.forEach((entry) => {
+			entry.isIntersecting ? localStorage
+				.setItem('selected-widget-style', entry.target.id) : null;
+			// console.log(entry.target.id)
+			// console.log(entry.intersectionRatio)
+		});
+	};
 
-    const observer = new IntersectionObserver(callback, options);
+	const observer = new IntersectionObserver(callback, options);
 
-    for (const target of targetClasses) {
-        observer.observe(target);
-    }
+	for (const target of targetClasses) {
+		observer.observe(target);
+	}
 };
