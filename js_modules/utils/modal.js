a686fac84d36a88b4c71541afe3a32d86a6f62cd
lscambo13 <ls.cambo13@gmail.com>
2023-12-27 19:01:04 +0530

    dev: Format code according to ES6

--- js_modules/utils/modal.js
+++ js_modules/utils/modal.js
@@ -1,49 +1,67 @@
-export const createModal = (title = 'Modal Title', inputFields = ['Input A', 'Input B']) => {
+export const createModal = (
+	title = 'Modal Title',
+	inputFields = ['Input A', 'Input B'],
+) => {
 	document.body
-		.insertAdjacentHTML("afterbegin", `
+		.insertAdjacentHTML('afterbegin', `
 		<div id="modalContainer" class="modalContainer">
 			<form action="" class="modal">
 				<h3 id="modalTitle" class="modalTitle">${title}</h3>
 				<div id="modalButtonsBar" class="modalButtonsBar">
-					<button id="modalSubmitButton" class="mainButton" onclick="" type="button">Submit</button>
-					<button id="modalCancelButton" class="" onclick="" type="button">Cancel</button>
+					<button
+					 id="modalSubmitButton"
+					 class="mainButton"
+					 onclick=""
+					 type="button">Submit</button>
+					<button
+					 id="modalCancelButton"
+					 class=""
+					 onclick=""
+					 type="button">Cancel</button>
 				</div>
 				<form>
 		</div>
-	`)
+	`);
 	inputFields.forEach((e) => {
-		let id = e.replaceAll(' ', '-')
-		document.getElementById('modalButtonsBar').insertAdjacentHTML("beforebegin", `
-					<label class="label" for="${id}">${e}</label>
-					<input class="inputField" autocomplete="off" type="text" id="${id}">
-		`)
-	})
+		const id = e.replaceAll(' ', '-');
+		document.getElementById('modalButtonsBar')
+			.insertAdjacentHTML('beforebegin', `
+					<label
+					 class="label"
+					 for="${id}">${e}</label>
+					<input
+					 class="inputField"
+					 autocomplete="off"
+					 type="text"
+					 id="${id}">
+		`);
+	});
 
-	let promise = new Promise((resolve, reject) => {
-		let modalContainer = document.getElementById('modalContainer')
-		let modalSubmitButton = document.getElementById('modalSubmitButton')
-		let modalCancelButton = document.getElementById('modalCancelButton')
-		let inputFields = document.getElementsByClassName('inputField')
-		inputFields[0].focus()
+	const promise = new Promise((resolve, reject) => {
+		const modalContainer = document.getElementById('modalContainer');
+		const modalSubmitButton = document.getElementById('modalSubmitButton');
+		const modalCancelButton = document.getElementById('modalCancelButton');
+		const inputFields = document.getElementsByClassName('inputField');
+		inputFields[0].focus();
 
 		const rejectModal = () => {
-			modalCancelButton.removeEventListener('click', rejectModal)
-			modalContainer.remove()
-			reject('Dialog was closed by the user')
-		}
+			modalCancelButton.removeEventListener('click', rejectModal);
+			modalContainer.remove();
+			reject(Error(null));
+		};
 
 		const resolveModal = () => {
 			modalSubmitButton.removeEventListener('click', resolveModal)
-			let result = []
+			const result = [];
 			for (const e of inputFields) {
-				result.push(e.value)
+				result.push(e.value);
 			}
-			modalContainer.remove()
-			resolve(result)
-		}
-		modalCancelButton.addEventListener('click', rejectModal)
-		modalSubmitButton.addEventListener('click', resolveModal)
-	})
+			modalContainer.remove();
+			resolve(result);
+		};
+		modalCancelButton.addEventListener('click', rejectModal);
+		modalSubmitButton.addEventListener('click', resolveModal);
+	});
 
-	return promise
-}
+	return promise;
+};
