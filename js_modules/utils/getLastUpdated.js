e8e8475b5c580dfd8d0c076ece1881094a4ce44b
lscambo13 <ls.cambo13@gmail.com>
2023-12-30 00:42:28 +0530

    dev: Handle api errors properly

--- js_modules/utils/getLastUpdated.js
+++ js_modules/utils/getLastUpdated.js
@@ -1,16 +1,18 @@
-export async function getLastUpdated(id) {
-	let response = await fetch(
-		'https://api.github.com/repos/lscambo13/casamia/commits/HEAD',
-	);
-	response = await response.json();
-	const date = new Date(response.commit.committer.date);
-	let time = date.toISOString();
-	time = time.slice(0, time.length - 5)
-	time = time
-		.replaceAll('T', '-')
-		.replaceAll(':', '')
-		.replaceAll('-', '.');
-	const element = document.getElementById(id);
-	element.innerHTML =
-		`Version ${time}`;
+export function getLastUpdated(id) {
+	fetch('https://api.github.com/repos/lscambo13/casamia/commits/HEAD')
+		.then((response) => {
+			response.json().then((response) => {
+				const date = new Date(response.commit.committer.date);
+				let time = date.toISOString();
+				time = time.slice(0, time.length - 5);
+				time = time
+					.replaceAll('T', '-')
+					.replaceAll(':', '')
+					.replaceAll('-', '.');
+				const element = document.getElementById(id);
+				element.innerHTML = `Version ${time}`;
+			});
+		}).catch((e) => {
+			console.error('getLastUpdated', e);
+		});
 }
