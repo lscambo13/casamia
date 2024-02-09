488c0accc28167fbb4644db0ca961393ccac0909
lscambo13 <ls.cambo13@gmail.com>
2024-02-10 00:03:31 +0530

    dev: Format code

--- js_modules/utils/letItSnow.js
+++ js_modules/utils/letItSnow.js
@@ -1,98 +1,103 @@
-let pause = false
-export function pauseSnaowfall(boolean) {
-	pause = boolean
-	isItChristmas()
-}
+let pause = false;
+// export function pauseSnowfall(boolean) {
+// 	pause = boolean;
+// 	isItChristmas();
+// }
+
 export function isItChristmas() {
-	let date = new Date()
+	const date = new Date();
 	if (date.getMonth() == 11 && date.getDate() > 17 && date.getDate() < 32) {
-		// console.log(date.getMonth() == 11, date.getDate() > 17, date.getDate() < 32)
-		letItSnow()
+		letItSnow();
 	}
 }
-function letItSnow() {
-	pause = true
-	//canvas init
-	var canvas = document.getElementById("canvasFar");
-	var ctx = canvas.getContext("2d");
 
-	//canvas dimensions
-	var W = window.innerWidth;
-	var H = window.innerHeight;
+function letItSnow() {
+	// pause = true;
+	//  canvas init
+	const canvas = document.getElementById('canvasFar');
+	const ctx = canvas.getContext('2d');
+
+	//  canvas dimensions
+	const W = window.innerWidth;
+	const H = window.innerHeight;
 	canvas.width = W;
 	canvas.height = H;
 
-	//snowflake particles
-	var mp = 50; //max particles
-	var particles = [];
-	for (var i = 0; i < mp; i++) {
+	//  snowflake particles
+	const mp = 50; //  max particles
+	const particles = [];
+	for (let i = 0; i < mp; i++) {
 		particles.push({
-			x: Math.random() * W, //x-coordinate
-			y: Math.random() * H, //y-coordinate
-			r: Math.random() * 4 + 1, //radius
-			d: Math.random() * mp //density
-		})
+			x: Math.random() * W, // x-coordinate
+			y: Math.random() * H, // y-coordinate
+			r: Math.random() * 4 + 1, // radius
+			d: Math.random() * mp, // density
+		});
 	}
 
-	//Lets draw the flakes
-	ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+	// Lets draw the flakes
+	ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
 	function draw() {
-		// let tStart = Date.now()
+		//  let tStart = Date.now()
 		ctx.clearRect(0, 0, W, H);
 		ctx.beginPath();
-		for (var i = 0; i < mp; i++) {
-			var p = particles[i];
+		for (let i = 0; i < mp; i++) {
+			const p = particles[i];
 			ctx.moveTo(p.x, p.y);
 			ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
 		}
 		ctx.fill();
 		update();
 		if (!pause) window.requestAnimationFrame(draw);
-		// console.log(Date.now() - tStart)
-		// console.log('draw')
+		//  console.log(Date.now() - tStart)
+		//  console.log('draw')
 	}
 
-	//Function to move the snowflakes
-	//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
-	// var angle = 0;
+	// Function to move the snowflakes
+	// angle will be an ongoing incremental flag.
+	// Sin and Cos functions will be applied to it to create vertical
+	// and horizontal movements of the flakes
+	//  let angle = 0;
 	function update() {
-		// console.log('update main')
-		// angle += 0.01;
-		for (var i = 0; i < mp; i++) {
-			// console.log('update loop')
-			var p = particles[i];
-			//Updating X and Y coordinates
-			//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
-			//Every particle has its own density which can be used to make the downward movement different for each flake
-			//Lets make it more random by adding in the radius
+		//  console.log('update main')
+		//  angle += 0.01;
+		for (let i = 0; i < mp; i++) {
+			//  console.log('update loop')
+			const p = particles[i];
+			// Updating X and Y coordinates
+			// We will add 1 to the cos function to prevent negative values
+			// which will lead flakes to move upwards
+			// Every particle has its own density which can be used to make
+			// the downward movement different for each flake
+			// Lets make it more random by adding in the radius
 			p.y += Math.cos(p.d) + 1 + p.r / 10;
-			// p.y += Math.cos(angle + p.d) + 1 + p.r / 2;
-			// p.x += Math.sin(angle) * 2;
+			//  p.y += Math.cos(angle + p.d) + 1 + p.r / 2;
+			//  p.x += Math.sin(angle) * 2;
 
-			//Sending flakes back from the top when it exits
-			//Lets make it a bit more organic and let flakes enter from the left and right also.
+			// Sending flakes back from the top when it exits
+			// Lets make it a bit more organic
+			// and let flakes enter from the left and right also.
 			if (p.x > W + 5 || p.x < -5 || p.y > H) {
-				if (i % 3 > 0) //66.67% of the flakes
-				{
+				if (i % 3 > 0) { // 66.67% of the flakes
 					particles[i] = { x: Math.random() * W, y: -10, r: p.r, d: p.d };
 				}
-				// else {
-				// 	//If the flake is exitting from the right
-				// 	if (Math.sin(angle) > 0) {
-				// 		//Enter from the left
-				// 		particles[i] = { x: -5, y: Math.random() * H, r: p.r, d: p.d };
-				// 	}
-				// 	else {
-				// 		//Enter from the right
-				// 		particles[i] = { x: W + 5, y: Math.random() * H, r: p.r, d: p.d };
-				// 	}
-				// }
+				//  else {
+				//  // If the flake is exiting from the right
+				//  if (Math.sin(angle) > 0) {
+				//  // Enter from the left
+				//  particles[i] = { x: -5, y: Math.random() * H, r: p.r, d: p.d };
+				//  }
+				//  else {
+				//  // Enter from the right
+				//  particles[i] = { x: W + 5, y: Math.random() * H, r: p.r, d: p.d };
+				//  }
+				//  }
 			}
 		}
 	}
-	canvas.style.display = 'block'
-	pause = false
+	canvas.style.display = 'block';
+	pause = false;
 	window.requestAnimationFrame(draw);
-	//animation loop
-	// setInterval(draw, 60);
+	// animation loop
+	//  setInterval(draw, 60);
 }
